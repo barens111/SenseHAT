98b24d1b85b3e16f9f1b09a4a8136de1ec299a74
gross432 <gross432@lsr7.net>
2018-12-10 09:07:06 -0600

    More mazes and win counter

--- marble_maze_bArens.py
+++ marble_maze_bArens.py
@@ -1,6 +1,9 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 from time import sleep
+import random
+
+
 false = False
 true = True
 r = (0,0,255)
@@ -8,8 +11,11 @@ w = (255,255,255)
 b = (0,0,0)
 k = (255,0,0)
 g = (0,255,0)
-sense.clear(w)
-maze = [
+wins = 0
+losses = 0
+
+
+maze1 = [
     [r,r,r,r,r,r,r,r,],
     [r,b,b,r,b,b,b,r,],
     [r,b,r,r,b,r,b,r,],
@@ -19,7 +25,28 @@ maze = [
     [r,k,b,b,b,b,b,r,],
     [r,r,r,r,r,r,r,r,],
     ]
-
+maze2 = [
+    [r,r,r,r,r,r,r,r,],
+    [r,b,r,k,r,k,b,r,],
+    [r,b,r,b,r,r,b,r,],
+    [r,b,r,b,b,b,b,r,],
+    [r,b,b,b,r,k,b,r,],
+    [r,b,r,b,r,r,b,r,],
+    [r,k,r,b,r,g,b,r,],
+    [r,r,r,r,r,r,r,r,],
+    ]
+maze3 = [
+    [r,r,r,r,r,r,r,r,],
+    [r,b,b,b,b,b,b,r,],
+    [r,b,b,b,b,b,b,r,],
+    [r,b,b,b,b,b,b,r,],
+    [r,b,b,b,b,b,b,r,],
+    [r,b,b,b,b,b,b,r,],
+    [r,b,b,b,b,b,g,r,],
+    [r,r,r,r,r,r,r,r,],
+    ]
+mazes = [maze1,maze2,maze3]
+maze = random.choice(mazes)
 y=1
 x=1
 nX=1
@@ -27,21 +54,21 @@ nY=1
 gameOver = False
 win = False
 while gameOver == False:
-    if not win or not gameOver:
-        sense.set_pixels(sum(maze,[]))
     maze[y][x] = w
     o = sense.get_orientation()
     pitch = o["pitch"]
     roll = o["roll"]
     yaw = o["yaw"]
+    sleep(0.0625
+          )
     if 10 < pitch and pitch < 170:
-        if maze[y][x-1]!=r:
+        if maze[y][x-1]==b:
             maze[y][x]=b
             x -= 1
         if maze[y][x-1]==g:
             maze[y][x]=b
             x -= 1
-            win=true
+            win = true
         if maze[y][x-1]==k:
             maze[y][x]=b
             x -= 1
@@ -50,11 +77,11 @@ while gameOver == False:
         if maze[y][x+1]==b:
             maze[y][x]=b
             x += 1
-        if maze[1][x+1]==g:
-            maze[y][x]=b
+        if maze[1][x+1]== g:
+            maze[y][x]= b
             x += 1
-            win=true
-        if maze[y][x+1]==k:
+            win = true
+        if maze[y][x+1]== k:
             maze[y][x]=b
             x += 1
             gameOver=true
@@ -69,7 +96,6 @@ while gameOver == False:
         if maze[y+1][x]==k:
             maze[y][x]=b
             y += 1
-
             gameOver=true
     if 170 < roll and roll < 350:
         if maze[y-1][x]==b:
@@ -78,22 +104,31 @@ while gameOver == False:
         if maze[y-1][x]==g:
             maze[y][x]=b
             y -= 1
-            win=true
+            win = true
         if maze[y-1][x]==k:
             maze[y][x]=b
             y -= 1
             gameOver=true
     maze[y][x] = w
-    if not win or not gameOver:
+    if not win and not gameOver:
         sense.set_pixels(sum(maze,[]))
     if win:
-        print('Win')
-        sense.clear(g)
-        sleep(0.125)
+        wins += 1
+        maze[y][x] = g
+        sense.show_letter(str(wins),w,g)
+        sleep(1)
+        maze = random.choice(mazes)
+        win = false
+        x=1
+        y=1
+        sense.set_pixels(sum(maze,[]))
+        sleep(.5)
     if gameOver:
-        print('lose')
-        sense.clear(k)
-
-
-
-        
+        losses += 1
+        maze[y][x]=k
+        sense.show_letter(str(losses),w,k)
+        sleep(1)
+        gameOver = false
+        x=1
+        y=1
+        sense.set_pixels(sum(maze,[]))
