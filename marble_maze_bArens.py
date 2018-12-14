a445c2844d02391417cafea3c8a7d4516044dda8
Austin <abarr328@lsr7.net>
2018-12-14 08:55:42 -0600

    More mazes Less Bugs

--- marble_maze_bArens.py
+++ marble_maze_bArens.py
@@ -14,7 +14,11 @@ g = (0,255,0)
 wins = 0
 losses = 0
 
-
+def checkIfWall(newX, newY):
+    if newX <= 8 and newY <= 8:
+        return True
+    else:
+        return False
 maze1 = [
     [r,r,r,r,r,r,r,r,],
     [r,b,b,r,b,b,b,r,],
@@ -77,46 +81,26 @@ maze6 = [
     ]
 maze7 = [
     [r,r,r,r,r,r,r,r,],
-    [r,b,r,b,b,b,b,r,],
-    [r,b,r,b,r,b,b,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,b,b,r,b,b,k,],
-    [r,b,r,b,r,g,g,r,],
-    [r,k,r,r,r,r,r,r,]
+    [r,b,b,b,b,b,b,r,],
+    [r,b,r,b,r,b,r,r,],
+    [r,k,r,b,r,b,b,r,],
+    [r,b,r,k,r,r,b,r,],
+    [r,b,b,b,b,b,b,k,],
+    [r,b,b,k,r,b,r,r,],
+    [r,r,g,r,r,r,r,r,]
     ]
 maze8 = [
     [r,r,r,r,r,r,r,r,],
-    [r,b,r,b,b,b,b,r,],
-    [r,b,r,b,r,b,b,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,b,b,r,b,b,k,],
-    [r,b,r,b,r,g,g,r,],
-    [r,k,r,r,r,r,r,r,]
+    [r,b,b,k,b,b,g,r,],
+    [r,b,r,r,b,k,r,r,],
+    [r,b,r,k,b,b,r,r,],
+    [r,b,r,r,r,b,b,r,],
+    [r,b,k,k,k,r,b,k,],
+    [r,b,b,b,b,b,b,r,],
+    [r,r,r,r,r,r,r,r,]
     ]
-maze9 = [
-    [r,r,r,r,r,r,r,r,],
-    [r,b,r,b,b,b,b,r,],
-    [r,b,r,b,r,b,b,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,b,b,r,b,b,k,],
-    [r,b,r,b,r,g,g,r,],
-    [r,k,r,r,r,r,r,r,]
-    ]
-maze10 = [
-    [r,r,r,r,r,r,r,r,],
-    [r,b,r,b,b,b,b,r,],
-    [r,b,r,b,r,b,b,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,r,b,k,b,r,r,],
-    [r,b,b,b,r,b,b,k,],
-    [r,b,r,b,r,g,g,r,],
-    [r,k,r,r,r,r,r,r,]
-    ]
-mazes = [maze1,maze2,maze3,maze4,maze5]
-mazes = [maze6]
+
+mazes = [maze1,maze2,maze3,maze4,maze5,maze6,maze7,maze8,]
 maze = mazes.pop(random.randint(0,(len(mazes)-1)))
 y=1
 x=1
@@ -130,61 +114,67 @@ while gameOver == False:
     yaw = o["yaw"]
     if 10 < pitch and pitch < 170:
         if not gameOver and not win:
-            if maze[y][x-1]==b:
-                maze[y][x]=b
-                x -= 1
-            if maze[y][x-1]== g:
-                maze[y][x]=b
-                x -= 1
-                win = True
-            if maze[y][x-1]==k:
-                maze[y][x]=b
-                x -= 1
-                gameOver=true
+            if checkIfWall(x-1, y) == True:
+                if maze[y][x-1]==b:
+                    maze[y][x]=b
+                    x -= 1
+                elif maze[y][x-1]== g:
+                    maze[y][x]=b
+                    x -= 1
+                    win = True
+                elif maze[y][x-1]==k:
+                    maze[y][x]=b
+                    x -= 1
+                    gameOver=true
+                sense.set_pixels(sum(maze,[]))
     if 170 < pitch and pitch < 350:
         if not gameOver and not win:
-            if maze[y][x+1]== b:
-                maze[y][x]= b
-                x += 1
-            if maze[y][x+1]== g:
-                maze[y][x]= b
-                x += 1
-                win = True
-                print(' hi ')
-            if maze[y][x+1]== k:
-                maze[y][x]=b
-                x += 1
-                gameOver = True
+            if checkIfWall(x+1, y) == True:
+                if maze[y][x+1]== b:
+                    maze[y][x]= b
+                    x += 1
+                elif maze[y][x+1]== g:
+                    maze[y][x]= b
+                    x += 1
+                    win = True
+                elif maze[y][x+1]== k:
+                    maze[y][x]=b
+                    x += 1
+                    gameOver = True
+                sense.set_pixels(sum(maze,[]))
     if 10 < roll and roll < 170:
         if not gameOver and not win:
-            if maze[y+1][x]== b:
-                maze[y][x]=b
-                y += 1
-            if maze[y+1][x]==g:
-                maze[y][x]=b
-                y += 1
-                win=true
-            if maze[y+1][x]==k:
-                maze[y][x]=b
-                y += 1
-                gameOver=true
+            if checkIfWall(x, y+1) == True:
+                if maze[y+1][x]== b:
+                    maze[y][x]=b
+                    y += 1
+                elif maze[y+1][x]==g:
+                    maze[y][x]=b
+                    y += 1
+                    win=true
+                elif maze[y+1][x]==k:
+                    maze[y][x]=b
+                    y += 1
+                    gameOver=true
+                sense.set_pixels(sum(maze,[]))
     if 170 < roll and roll < 350:
         if not gameOver and not win:
-            if maze[y-1][x]==b:
-                maze[y][x]=b
-                y -= 1
-            if maze[y-1][x]==g:
-                maze[y][x]=b
-                y -= 1
-                win = true
-            if maze[y-1][x]==k:
-                maze[y][x]=b
-                y -= 1
-                gameOver=true
+            if checkIfWall(x, y-1) == True:
+                if maze[y-1][x]==b:
+                    maze[y][x]=b
+                    y -= 1
+                elif maze[y-1][x]==g:
+                    maze[y][x]=b
+                    y -= 1
+                    win = true
+                elif maze[y-1][x]==k:
+                    maze[y][x]=b
+                    y -= 1
+                    gameOver=true
+                sense.set_pixels(sum(maze,[]))
     maze[y][x] = w
     if not win and not gameOver:
         sense.set_pixels(sum(maze,[]))
-        sleep(.125)
     if win:
         sense.set_pixels(sum(maze,[]))
         sleep(.125)
@@ -197,8 +187,7 @@ while gameOver == False:
             sense.show_letter(str(wins),w,g)
             sleep(1)
             maze = mazes.pop(random.randint(0,(len(mazes))-1))
-            print(len(mazes))
-            
+    
             win = false
             x=1
             y=1
